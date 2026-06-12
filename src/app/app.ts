8e519c74fb87d191bfb92b9ea2e3ed0a7d8a921b
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2026-06-12 16:32:30 -0300

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Two-Way-Data-Binding');
+  name: string = 'Gabriel';
 }
